d8c424401036293b7c86d8e38ca26cf3f4caff0b
JCACD <Joestar.Chang@accordance.com.tw>
2026-06-23 13:57:51 +0800

    Implement random bar display in app_main
    Added a visual representation of random levels using bars.

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_log.h"
 
 void app_main(void)
 {
-    printf("Hello world!\n");
-    ESP_LOGI("main", "ESP32-S3 N32R16V is running with 32MB Flash & 16MB PSRAM!");
-    
-    while(1) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+    const char *bars[] = {
+        "▁",
+        "▂",
+        "▃",
+        "▄",
+        "▅",
+        "▆",
+        "▇",
+        "█"
+    };
+
+    int level[16];
+
+    while (1) {
+
+        printf("\r");
+
+        for(int i=0;i<16;i++)
+        {
+            level[i] = esp_random() % 8;
+            printf("%s ", bars[level[i]]);
+        }
+
+        fflush(stdout);
+
+        vTaskDelay(pdMS_TO_TICKS(120));
     }
 }
+
+// #include <stdio.h>
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
+// #include "esp_log.h"
+
+// void app_main(void)
+// {
+//     printf("Hello world!\n");
+//     ESP_LOGI("main", "ESP32-S3 N32R16V is running with 32MB Flash & 16MB PSRAM!");
+    
+//     while(1) {
+//         vTaskDelay(pdMS_TO_TICKS(1000));
+//     }
+// }
